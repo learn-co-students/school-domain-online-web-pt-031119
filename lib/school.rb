a1c8810e6bd5c305f225fbe37054b1d5fe8dc6a5
Mark Kanko <mkanko66@gmail.com>
2019-03-27 22:43:50 -0700

    completed oo basics with class constants lab

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,38 @@
-# code here!
+require 'pry'
+
+class School
+
+    attr_accessor :name, :student
+
+    def initialize(roster)
+        @roster = {}
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(student, grade)
+       if @roster[grade]
+    #    binding.pry 
+          @roster[grade] << student
+    #      binding.pry 
+       else
+          @roster[grade] = [student] 
+    #      binding.pry 
+       end
+    end 
+
+    def grade(grade)
+        @roster[grade]  
+    end
+
+    def sort
+        sorted_hash = {}
+        @roster.each do |grade, names_array|
+            sorted_hash[grade] = names_array.sort
+        end
+        sorted_hash  
+    end 
+
+end 
