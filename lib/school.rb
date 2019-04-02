c0c4b1201f28e63f05696f06e95feb840406c3c3
Daniel Lima <daniel.echuaca.lima@gmail.com>
2019-04-02 16:07:34 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+class School
+  
+  def initialize (name)
+    @name = name
+    @roster = {}
+  end
+
+
+ def roster
+  @roster
+ end
+
+ def add_student (person, grade)
+   @roster[grade] ||= []
+   @roster[grade] << person
+ end
+ 
+ def grade (level)
+   @roster[level]
+ end
+ 
+ def sort
+   new_hash = {}
+   @roster.sort_by do |x, y|
+     new_hash[x] = y.sort
+   end
+   new_hash
+ end
+
+end
